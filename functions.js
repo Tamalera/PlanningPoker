478af9f9751f70e493e1e6da10235152527abb45
Kaesler Niklas <Niklas.Kaesler@swisscom.com>
2018-09-18 14:40:52 +0200

    MieP pour pascla pour evalue le max e le average avec la fibonaccizahlen

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+var x = 0;
 function ausWahlBestaetigen() {
     var flipCard = document.getElementById("cardWrapper").children;
     var txt = "";
@@ -24,4 +25,37 @@ function resetPoints(){
     for (var i =0; i < array.length; i++) {
         array[i].value = "";
     }
+    document.getElementById("totalpoints").innerHTML = "";
+    document.getElementById("average").innerHTML = "";
+}
+
+function updatevalue(){
+    var totalvalue = 0;
+    var count = 0;
+    var fibos = [0,1,2,3,5,8,13,21,34,55,89,144]
+    var array = document.getElementsByClassName("storyPointInput");
+    for (var i =0; i < array.length; i++) {
+        if( array[i].value != ""){
+        
+        totalvalue += +array[i].value;
+        count += 1;
+        }
+    document.getElementById("totalpoints").innerHTML = totalvalue;
+    console.log(totalvalue, "lol");
+    var average = totalvalue / count;
+    for (var i =0; i < fibos.length; i++){
+        if( average == fibos[i]){
+            x = fibos[i];
+            console.log("fuck this shit");
+        }
+        else if(average > fibos[i] && average < fibos[i+1]) {
+            console.log(average, fibos[i], fibos[i+1])
+            x = fibos[i+1];
+        }
+        else{
+            console.log("mieep");
+        }
+    }
+    document.getElementById("average").innerHTML = x;
+}
 }
