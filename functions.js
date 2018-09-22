f425cc3d467da3c0d2aa8d12e37f498168088c0b
tamalera <florence.meier@alumni.ethz.ch>
2018-09-22 17:56:26 +0200

    Zusammenfasung wird als tabelle erstellt

--- functions.js
+++ functions.js
@@ -45,7 +45,7 @@ function updatevalue(){
         
         totalvalue += +array[i].value;
         count += 1;
-        }
+    }
     document.getElementById("totalpoints").innerHTML = totalvalue;
     console.log(totalvalue, "lol");
     var average = totalvalue / count;
@@ -68,19 +68,73 @@ function updatevalue(){
 /* END AVERAGE PUNKTE */
 
 /* ZUSAMMENFASSUNG ANZEIGEN */
-function toggleExport(){
+function erstelleZuFas(){
+    /* Toggle "Export to PDF" btn */
     var exportButton = document.getElementById("exportButton");
     if (exportButton.hidden === true) {
         exportButton.hidden = false;
     } else {
         exportButton.hidden = true;
     }
-    /* TODO ZUSAMMENFASSUNG ANZEIGEN... */
-}
 
+    /* ZUSAMMENFASSUNG ANZEIGEN: Erstmal Table-Div anzeigen */
+    var tableContainer = document.getElementById("tableContainer");
+    if (tableContainer.hidden === true) {
+        tableContainer.hidden = false;
+    } else {
+        tableContainer.hidden = true;
+    }
+
+    /* Sicherstellen, dass Tabelle nicht existiert, sonst doppelt */
+    var tabelle = document.getElementById('zusammenfassungTabelle');
+    if (typeof tabelle != 'undefined' && tabelle != null)
+    {
+        tabelle.parentElement.removeChild(tabelle);
+    }
+
+    var container = document.getElementById('tableContainer');
+    var table = document.createElement('table');
+    table.className = "table m-4";
+    table.id ="zusammenfassungTabelle";
+    var tableHead =  document.createElement('thead');
+    tableHead.className = "thead-light";
+    var headRow = document.createElement('tr');
+    tableHead.appendChild(headRow);
+    for (var index = 0; index < 2; index++) {
+        var th_td = document.createElement('td');
+        if (index === 0) {
+            th_td.textContent = "Story Points";
+        }
+        else {
+            th_td.textContent = "Teammitglied";
+        }
+        th_td.appendChild(document.createTextNode('\u0020'));
+        headRow.appendChild(th_td);
+    }
+    var tableBody = document.createElement('tbody');
+    var array = document.getElementsByClassName("teammitglied");
+    for (var i = 0; i < array.length; i++) {
+        var tr = document.createElement('tr');
+        for (var j = 0; j < 2; j++) {
+            var td = document.createElement('td');
+            if (j === 0) {
+                td.textContent = array[i].children[0].value.toString();
+            }
+            else {
+                td.textContent = array[i].children[1].value.toString();
+            }
+            td.appendChild(document.createTextNode('\u0020'))
+            tr.appendChild(td)
+        }
+        tableBody.appendChild(tr);
+    }
+    table.appendChild(tableHead);
+    table.appendChild(tableBody);
+    container.appendChild(table)
+}
 /* ENDE ZUSAMMENFASSUNG ANZEIGEN */
 
-/* DRUCKEN */
+/* DRUCKEN: Wird momentan nicht benötigt... */
 function drucken(title) {
     let fenster = window.open('', 'PRINT', 'height=650,width=900,top=100,left=150');
     
@@ -103,9 +157,9 @@ function drucken(title) {
 /* EXPORT ALS PDF */
 function exportAlsPdf() {
     var zusammenfassung = new jsPDF();
+    zusammenfassung.setFillColor(1);
     /* Inhalt: */
-    zusammenfassung.addHTML($('#storyPoints').first(),function(){
-
+    zusammenfassung.addHTML($('#tableContainer').first(),function(){
          /* Direkt in Chrome als pdf anzeigen */
         zusammenfassung.autoPrint()
 
